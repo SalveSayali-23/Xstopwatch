525bd5afa109c98e2c880b83b521bbbe6d17bd2d
Sayali <salvesayali23@gmail.com>
2024-04-02 02:30:10 +0530

    changes done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,46 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useState, useEffect } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [timer, setTimer] = useState(0);
+  const [isRunning, setIsRunning] = useState(false);
 
+  useEffect(() => {
+    let timerId;
+    if(isRunning){
+       timerId = setInterval(() => {
+      setTimer((prevTimer) => prevTimer + 1);
+    }, 1000);
+  }
+    return () => {
+      clearInterval(timerId);
+    };
+  }, [isRunning, timer]);
+
+    
+    
+  const formatTime = (secs) => {
+    const mins = Math.floor(secs / 60);
+    const remainingSecs = secs % 60;
+    return `${mins}:${remainingSecs < 10 ? "0" : ""}${remainingSecs}`;
+  };
+
+  const startReset = () => {
+    setIsRunning(!isRunning);
+  };
+
+  const resetHandler = () => {
+    setTimer(0);
+    setIsRunning(false);
+  };
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Stopwatch</h1>
+      <p>Time : {formatTime(timer)}</p>
+      <button onClick={startReset}>{isRunning ? "Stop" : "Start"}</button>
+      <button onClick={resetHandler}>Reset</button>
+    </div>
+  );
+};
 
-export default App
+export default App;
